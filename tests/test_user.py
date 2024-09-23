5725d3abfd65dfa916e42c1d69974a8fb53f4d8e
Rogelio E <100113445+ushio-343@users.noreply.github.com>
2024-09-22 20:53:41 -0600

    ci-pipeline2

--- tests/test_user.py
+++ tests/test_user.py
@@ -7,21 +7,21 @@ def test_create_user(client, db_session):
 def test_read_user(client, db_session):
     # Primero, crea un usuario
     client.post("/api/v1/users/", json={"name": "Jane Doe", "email": "jane@example.com"})
-    response = client.get("/users/1/")
+    response = client.get("/api/v1/users/1/")
     assert response.status_code == 200
     assert response.json() == {"id": 1, "name": "Jane Doe", "email": "jane@example.com"}
 
 def test_update_user(client, db_session):
     # Primero, crea un usuario
     client.post("/api/v1//users/", json={"name": "Alice", "email": "alice@example.com"})
-    response = client.put("/users/1/", json={"name": "Alice Smith", "email": "alice.smith@example.com"})
+    response = client.put("/api/v1/users/1/", json={"name": "Alice Smith", "email": "alice.smith@example.com"})
     assert response.status_code == 200
     assert response.json() == {"id": 1, "name": "Alice Smith", "email": "alice.smith@example.com"}
 
 def test_delete_user(client, db_session):
     # Primero, crea un usuario
     client.post("/api/v1/users/", json={"name": "Bob", "email": "bob@example.com"})
-    response = client.delete("/users/1/")
+    response = client.delete("/api/v1/users/1/")
     assert response.status_code == 200
-    response = client.get("/users/1/")
+    response = client.get("/api/v1/users/1/")
     assert response.status_code == 404
