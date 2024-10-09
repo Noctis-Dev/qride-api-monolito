48fedcc7aec09b7b37c7ba3413a8bfc34c89cb55
Mane <61853435+Mane-jaker@users.noreply.github.com>
2024-10-09 15:24:04 -0600

    env database

--- app/db.py
+++ app/db.py
@@ -1,9 +1,14 @@
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "mysql+pymysql://root:3a5n8g123A;@localhost/qride"
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+
+# Obtener la URL de la base de datos desde las variables de entorno
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -15,4 +20,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
